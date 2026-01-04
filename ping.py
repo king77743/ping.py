d96ecacbeee5bbfc58cdf7c35a20462fc620ea23
SpaceOfCyber <artemmatvienko154@gmail.com>
2026-01-04 17:39:19 +0500

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,11 +1,29 @@
 import scapy.all as scapy
 import time
+
+green='\033[92m'
+red='\033[91m'
+reset='\033[0m'
+cian='\033[36m'
+y="\033[93m"
+m='\033[1;36m'
+banner=r"""
+  _____ _____ _   _  _____   _______ ____   ____  _      
+ |  __ \_   _| \ | |/ ____| |__   __/ __ \ / __ \| |     
+ | |__) || | |  \| | |  __     | | | |  | | |  | | |     
+ |  ___/ | | | . ` | | |_ |    | | | |  | | |  | | |     
+ | |    _| |_| |\  | |__| |    | | | |__| | |__| | |____ 
+ |_|   |_____|_| \_|\_____|    |_|  \____/ \____/|______|
+                                                         
+                                                        
+"""
+print(f'{m}{banner}{reset}')
 try:
-    ip_t=input("введите ip:")
+    ip_t=input(f"{cian}[*]{reset} Введите ip:")
     try:
-        chislo=int(input("введите сколько пакетов будет отправлено:"))
+        chislo=int(input(f"{cian}[*]{reset} Введите сколько пакетов будет отправлено:"))
     except ValueError:
-        print("Введите число пакетов")
+        print(f"{y}[!]{reset} Введите число пакетов")
         exit()
     def ping(ip,count):
         p=scapy.Ether()/scapy.IP(dst=ip)/scapy.ICMP()
@@ -15,13 +33,13 @@ try:
                 count-=1
                 if ans:
                     if ans.haslayer(scapy.ICMP) and ans[scapy.ICMP].type==0:
-                        print(f"получен ответ от {ans[scapy.IP].src} тип:Echo reply")
+                        print(f"{green}[+]{reset} Получен ответ от {ans[scapy.IP].src} тип:Echo reply")
                 else:
-                    print(f"IP:{ip} не отвечает(возможно, адрес не существует или устройство выключено)")
+                    print(f"{red}[-] {reset}IP:{ip} не отвечает(возможно, адрес не существует или устройство выключено)")
                 if count>0:
                     time.sleep(1)
         except KeyboardInterrupt:
-            print("остановлено")
+            print(f"{y}[!]{reset}Остановлено")
     ping(ip_t,chislo)   
 except KeyboardInterrupt:
-    print("\nОстановлено")
+    print(f"\n{y}[!]{reset} Остановлено")
